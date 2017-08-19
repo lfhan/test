7514b9ee46b568a7a556c8d2e0cf9308c892eff4
lfhan <heb_sjz_hlf@163.com>
2017-08-19 14:05:29 +0800

    增加注釋！

--- test1/src/main/java/com/lfhan/test/Test.java
+++ test1/src/main/java/com/lfhan/test/Test.java
@@ -3,5 +3,7 @@ package com.lfhan.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("这个项目将托管到GitHub！");
+		System.out.println("提交到了GitHub，但是本地的jdk好像坏掉了，"
+				+ "爲什麽呢？難道是因爲，今天更新jre，然後沒更新成功？");
 	}
 }

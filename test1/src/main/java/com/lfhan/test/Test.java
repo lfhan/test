59aea4b81f0b512a8f9677a05195edc936f821b0
lfhan <heb_sjz_hlf@163.com>
2017-08-19 20:11:25 +0800

    1
    Change-Id: I8346c2b9c5a6417bec16bbb952b34da3d7e9a73a

--- test1/src/main/java/com/lfhan/test/Test.java
+++ test1/src/main/java/com/lfhan/test/Test.java
@@ -8,7 +8,6 @@ public class Test {
 		System.out.println("这个项目将托管到GitHub！");
 		System.out.println("提交到了GitHub，但是本地的jdk好像坏掉了，"
 				+ "爲什麽呢？難道是因爲，今天更新jre，然後沒更新成功？");
-		12
 		Date d = new Date();
 	}
 }
